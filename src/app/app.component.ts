874bae689eddda7898cad59a906718e19d7d7ab6
Guy Arbilly <Guy.Arbilly@gmail.com>
2025-03-16 21:52:27 +0200

    chapter 11 section 209 - using signal instead observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -8,19 +8,28 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
-
+  clickCount = signal<number>(0);
+  constructor(){
+    effect(()=> {
+      console.log(`button was clicked ${this.clickCount()} times`)
+    })
+  }
   ngOnInit(): void {
-    const subscription = interval(1000).pipe(
-      // we will pass a function to map and the new value wil be repalced as observalbe value
-      map((val) => val * 2)
-    ).subscribe({ //rxjs function that create observalbe
-      // if not subscribing the interval no observer will be created
-      // using pipe operator and then we can add more operators
-      next: (val) => console.log('val', val)
-    });
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe()
-    });
+    // const subscription = interval(1000).pipe(
+    //   // we will pass a function to map and the new value wil be repalced as observalbe value
+    //   map((val) => val * 2)
+    // ).subscribe({ //rxjs function that create observalbe
+    //   // if not subscribing the interval no observer will be created
+    //   // using pipe operator and then we can add more operators
+    //   next: (val) => console.log('val', val)
+    // });
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe()
+    // });
+  }
+  // using signal that will be updated in each click
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1)
   }
 
 }
