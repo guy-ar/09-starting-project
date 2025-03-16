7fd4b85c6a7d711b70c94e82acfee4721c05b040
Guy Arbilly <Guy.Arbilly@gmail.com>
2025-03-16 21:34:28 +0200

    chapter 11 section 208 - adding operators to observalbe (pipe and then map)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,12 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    const subscription = interval(1000).subscribe({ 
+    const subscription = interval(1000).pipe(
+      // we will pass a function to map and the new value wil be repalced as observalbe value
+      map((val) => val * 2)
+    ).subscribe({ //rxjs function that create observalbe
+      // if not subscribing the interval no observer will be created
+      // using pipe operator and then we can add more operators
       next: (val) => console.log('val', val)
     });
     this.destroyRef.onDestroy(() => {
